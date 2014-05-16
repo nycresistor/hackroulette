a0ef4e3647eb1257dd33affdfe61c5a9fe7c73f2
George Shammas <george@shamm.as>
2014-05-16 23:19:07 +0000

    go fmt

--- main.go
+++ main.go
@@ -1,48 +1,49 @@
 package main
 
 import (
-    "io"
-    "log"
-    "net"
-    "fmt"
+	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
 const listenAddr = ":4000"
+
 var telnetOneChar []byte = []byte("\377\375\042\377\373\001")
 
 var partner = make(chan io.ReadWriteCloser)
 
 func match(c io.ReadWriteCloser) {
-    fmt.Fprint(c, "Waiting for a partner...")
-    select {
-    case partner <- c:
-        // now handled by the other goroutine
-    case p := <-partner:
-        chat(p, c)
-    }
+	fmt.Fprint(c, "Waiting for a partner...")
+	select {
+	case partner <- c:
+		// now handled by the other goroutine
+	case p := <-partner:
+		chat(p, c)
+	}
 }
 
 func chat(a, b io.ReadWriteCloser) {
-    defer a.Close()
-    defer b.Close()
-    a.Write(telnetOneChar)
-    b.Write(telnetOneChar)
-    fmt.Fprintln(a, "Found one! Say hi.")
-    fmt.Fprintln(b, "Found one! Say hi.")
-    go io.Copy(a, b)
-    io.Copy(b, a)
+	defer a.Close()
+	defer b.Close()
+	a.Write(telnetOneChar)
+	b.Write(telnetOneChar)
+	fmt.Fprintln(a, "Found one! Say hi.")
+	fmt.Fprintln(b, "Found one! Say hi.")
+	go io.Copy(a, b)
+	io.Copy(b, a)
 }
 
 func main() {
-    l, err := net.Listen("tcp", listenAddr)
-    if err != nil {
-        log.Fatal(err)
-    }
-    for {
-        c, err := l.Accept()
-        if err != nil {
-            log.Fatal(err)
-        }
-        go match(c)
-    }
+	l, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go match(c)
+	}
 }
